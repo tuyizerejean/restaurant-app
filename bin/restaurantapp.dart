8f3451fe19b9c5b828a439e614a8406871790867
tuyizerejean <tuyibobu@gmail.com>
2021-10-08 14:54:43 -0700

    project setup

---
+++ bin/restaurantapp.dart
@@ -0,0 +1,5 @@
+import 'package:restaurantapp/restaurantapp.dart' as restaurantapp;
+
+void main(List<String> arguments) {
+  print('Hello world: ${restaurantapp.calculate()}!');
+}
---
+++ test/restaurantapp_test.dart
@@ -0,0 +1,8 @@
+import 'package:restaurantapp/restaurantapp.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
